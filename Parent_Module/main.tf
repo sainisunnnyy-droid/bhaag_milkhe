49331d392e37ca254a7d89c8b9ade67506a312f1
unknown <saini.sunnnyy@gmail.com>
2026-08-18 23:51:14 +0530

    NEW_RG

--- Parent_Module/main.tf
+++ Parent_Module/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg_ghewarwale" {
   name     = "rg-jamunwale"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "rg_gudwale" {
+  name     = "rg-gulabwale"
+  location = "East US"
+}
