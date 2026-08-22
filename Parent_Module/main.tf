c7aac2530012cf369739c68ca3759645e1ac4e08
unknown <saini.sunnnyy@gmail.com>
2026-08-22 17:31:50 +0530

    aalechakmeaagya

--- Parent_Module/main.tf
+++ Parent_Module/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg_lewariya" {
   name     = "rg-ghewariya"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "rg_tewariya" {
+  name     = "rg-jhewariya"
+  location = "East US"
+}
